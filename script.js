2620763fec7a7cc29e776f4288afb0ec21358bf5
Henrique DS03 <henrique_stolle@estudante.sesisenai.org.br>
2024-04-03 11:44:27 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function conta() {
+    var block = 'block';
+    document.getElementById('test').style.display = block;
+    document.getElementById('test').innerHTML = 9 + 18;
+
+};
+
+function funcaoMudarTamanhoGrande () {
+    var variavel = '2.5rem';
+    document.getElementById('test').style.fontSize = variavel;
+};
+
+function funcaoMudarTamanhoPequeno () {
+    var variavel = '1rem';
+    document.getElementById('test').style.fontSize = variavel;
+};
